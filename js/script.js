a35abf6b9ef2dd0200ad6d74ec33c6d299e00364
Анна Кучерявенкова <anya.shurshalova@mail.ru>
2021-04-20 11:42:33 +0400

    Оформляем вывод вопросов в html (дом дерево + что можно делать с элементами)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-let num; //для перехода между вопросами при увеличении значения переменной
-let score; //для учета набранных очков
-
 const questions = [
 	{
 		question: "Кто стал первым в истории человеком, который полетел в Космос?", 
@@ -39,20 +36,44 @@ const questions = [
 		correctAnswer: "a"
 	}
 ];
+let num; //для перехода между вопросами при увеличении значения переменной
+let score = 0; //для учета набранных очков
+let answer = ['b', 'b', 'c', 'b'];
+
+let questionItem = document.getElementById('question');
+questionItem.classList.add('wrap');
 
 // let answers = questions.filter(item => item.correctAnswer == item.c);
 // console.log(answers);
-
-let answer = ['b', 'b', 'c', 'b'];
 
 function checkAnswers(answer, questions) {
 	for (let i = 0; i < questions.length; i++) {
 		if (questions[i].correctAnswer == answer[i]) {
 			console.log('Ответ верный');
+			score++;
 		} else {
 			console.log('Ответ неверный');
-		}
-	}
+		};
+	};
 }
-
 checkAnswers(answer, questions);
+
+function buildQuiz(questions) {
+	questions.forEach(item => {
+		let newQuestion = document.createElement('h2');
+		newQuestion.classList.add('title');
+		newQuestion.innerHTML = item.question;
+		questionItem.appendChild(newQuestion);
+
+		let answerOptions = document.createElement('ol');
+		answerOptions.classList.add('list');
+		answerOptions.innerHTML = item.answers;
+		questionItem.appendChild(answerOptions);
+	});
+}
+buildQuiz(questions);
+
+let replyCounter = document.createElement('div');
+replyCounter.classList.add('counter');
+replyCounter.innerHTML = `Количество правильных ответов: ${score}`;
+questionItem.appendChild(replyCounter);
